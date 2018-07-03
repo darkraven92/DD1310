abfa43bd84d0c51d8a60c3a7768ccceb6f2bd53c
darkraven92 <36090181+darkraven92@users.noreply.github.com>
2018-07-03 18:08:47 +0200

    Se TODO

--- Laborationer/Laboration 2/Program.py
+++ Laborationer/Laboration 2/Program.py
@@ -30,3 +30,34 @@ def tabell():
         print("Det kommer att kosta",pris,"kr")
 
 #tabell()
+
+#Uppgift 3: Slingor
+
+# TODO Hitta felet med None
+def slingor():
+    antal_paket = int(input("Hur många paket vill du skicka:"))
+    counter = 0
+    summa_paket = 0
+
+    while counter < antal_paket:
+        counter += 1
+        vikt = int(input(print("Ange vikt för paket",counter,":")))
+
+        if vikt <= 2:
+            pris = vikt*30
+            summa_paket = summa_paket + pris
+        elif vikt > 2 and vikt <= 6:
+            pris = vikt*28
+            summa_paket = summa_paket + pris
+        elif vikt > 6 and vikt <= 12:
+            pris = vikt*25
+            summa_paket = summa_paket + pris
+        elif vikt > 12:
+            pris = vikt*23
+            summa_paket = summa_paket + pris
+
+    print("Det kommer att kosta",summa_paket,"kr")
+
+
+
+slingor()
