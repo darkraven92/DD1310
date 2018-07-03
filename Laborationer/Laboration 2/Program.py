a6ef1d55af8b2f7c7376bf8d3d2d363396d6c8aa
darkraven92 <36090181+darkraven92@users.noreply.github.com>
2018-07-03 16:59:56 +0200

    Upgift 2 klar

--- Laborationer/Laboration 2/Program.py
+++ Laborationer/Laboration 2/Program.py
@@ -10,4 +10,23 @@ def bransleforbrukning():
 
     print("Bränsleförbrukningen för bilen är: ",round(bf, 3))
 
-bransleforbrukning()
+#bransleforbrukning()
+
+#Uppgift 2: If-satser
+def tabell():
+
+    anv_input = float(input("Hur mycket väger paketet:"))
+    if anv_input <= 2:
+        pris = anv_input*30
+        print("Det kommer att kosta",pris,"kr")
+    elif anv_input > 2 and anv_input <=6:
+        pris = anv_input*28
+        print("Det kommer att kosta",pris,"kr")
+    elif anv_input > 6 and anv_input <= 12:
+        pris = anv_input*25
+        print("Det kommer att kosta",pris,"kr")
+    elif anv_input > 12:
+        pris = anv_input*23
+        print("Det kommer att kosta",pris,"kr")
+
+#tabell()
