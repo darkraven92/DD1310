b6e49b6b962fda70b89c36dab92e33dac297a3b4
darkraven92 <36090181+darkraven92@users.noreply.github.com>
2018-07-03 16:45:04 +0200

    Uppgift 1 klar

--- Laborationer/Laboration 2/Program.py
+++ Laborationer/Laboration 2/Program.py
@@ -1,2 +1,13 @@
 #Uppgift 1: Bränsleförbrukning
 
+def bransleforbrukning():
+
+    p = float(input("Bilens körsträcka i km: "))
+    fb = float(input("Hur mycket förbrukat bränsle i liter:"))
+
+
+    bf = (100*fb)/p
+
+    print("Bränsleförbrukningen för bilen är: ",round(bf, 3))
+
+bransleforbrukning()
