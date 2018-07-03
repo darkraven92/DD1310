f9b94a5c9dc6862556f8822d17f6ad019cb9e5df
darkraven92 <36090181+darkraven92@users.noreply.github.com>
2018-07-03 16:26:08 +0200

    Added code and comment

---
+++ Laborationer/Laboration 1- Introduktion till datormilj-n och Python/Program.py
@@ -0,0 +1,3 @@
+print("Hej") #Skriver ut hej i consolen
+namn = input("Vad heter du?") #Lägger till det namn som anv. skriver in i strängen namn
+print("Ohh",namn,"vilket fint namn") #Skriver ut "Ohh", hämtar strängen namn och sedan skriver ut "vilket fint namn"
