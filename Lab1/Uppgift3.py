40f2a84214afe4c107ef71b593ce52f37da2c261
darkraven92 <36090181+darkraven92@users.noreply.github.com>
2023-02-23 23:07:13 +0100

    Create Uppgift3.py

---
+++ Lab1/Uppgift3.py
@@ -0,0 +1,5 @@
+while True:
+    a = int(input("Ange ett heltal för a: "))
+    b = int(input("Ange ett heltal för b: "))
+    kubsumma = a**3 + b**3
+    print(f"Kubsumman av {a}³ och {b}³ är {kubsumma}.")
